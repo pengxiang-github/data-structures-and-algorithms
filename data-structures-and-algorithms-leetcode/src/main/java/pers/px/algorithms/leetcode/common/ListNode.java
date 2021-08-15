b1f42fb57459b26d6ea098636a89a5c637adce38
pengxiang <pengxiang@vipkid.com.cn>
2021-08-16 01:45:55 +0800

    Initial commit

---
+++ data-structures-and-algorithms-leetcode/src/main/java/pers/px/algorithms/leetcode/common/ListNode.java
@@ -0,0 +1,28 @@
+package pers.px.algorithms.leetcode.common;
+
+/**
+ * @Author : pengxiang
+ * @Date : 2021-08-16 00:38:00
+ * @Program : data-structures-and-algorithms
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
